22fb1f8eb4775bd3ddbe873e69615bb9e3c6cd73
pedrobraga <pedro.braga@me.com>
2025-05-12 15:31:44 +0100

    requirements

---
+++ venv_setup.sh
@@ -0,0 +1,15 @@
+python3 -m venv RepoSurverVENV
+source mcp_env/bin/activate
+
+# front-end libraries
+
+pip install dearpygui 
+pip install gitpython 
+pip install requests 
+pip install numpy 
+pip install datetime
+
+# MCP libraries
+pip install mcp 
+pip install autogen 
+pip install ag2[openai]
