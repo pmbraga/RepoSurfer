a76902a05208baaa11fd147cea8cb089c6a4594b
Your Name <meselfme6@gmail.com>
2025-05-12 16:04:52 +0100

    Update token and repo name

--- github_mcp.py
+++ github_mcp.py
@@ -1,47 +1,29 @@
 # mcp_server.py
 import argparse
 import os
-from pathlib import Path
 from typing import List
 from mcp.server.fastmcp import FastMCP
 
 from github import Github
 
+with open("mcp_init_info.txt", "r") as f:
+    auth_token = f.readline().strip()
+    repo_name = f.readline().strip()
+
 # Authentication is defined via github.Auth
 from github import Auth
-
-# using an access token
-auth = Auth.Token("ENTER YOUR TOKEN")
-
+auth = Auth.Token(auth_token)
 # Public Web Github
 g = Github(auth=auth)
 g.get_user().login
 
+# repo_name = "TEST_USER/TEST_REPO"
+
 # Initialize the MCP server
 mcp = FastMCP("FilesystemServer")
 
-# @mcp.tool()
-# def get_git_repos() -> List[str]:
-#     """
-#     Get a list of all repositories for the authenticated user.
-#     """
-#     repos = g.get_user().get_repos()
-#     return [repo.name for repo in repos]
-
 @mcp.tool()
-def get_repo_issues(repo_name: str) -> List[str]:
-    """
-    Get a list of all issues (including PRs) for a specific repository.
-    """
-    repo = g.get_repo(f'{repo_name}')
-
-    # Fetch issues (this also returns PRs)
-    all_items = repo.get_issues()
-
-    return [i.title for i in all_items]
-
-@mcp.tool()
-def get_repo_branches(repo_name: str) -> List[str]:
+def get_repo_branches() -> List[str]:
     """
     Get a list of all branches for a specific repository.
     """
@@ -53,7 +35,7 @@ def get_repo_branches(repo_name: str) -> List[str]:
     return [i.name for i in all_items]
 
 @mcp.tool()
-def get_repo_pulls(repo_name: str, state) -> List[str]:
+def get_repo_pulls(state) -> List[str]:
     """
     Get a list of pull requests for a specific repository. State can be open or closed.
     """
@@ -64,7 +46,7 @@ def get_repo_pulls(repo_name: str, state) -> List[str]:
     return [f"#{pr.number} {pr.title}  —  by {pr.user.login}  [{pr.state}]" for pr in pulls]
 
 @mcp.tool()
-def get_repo_issues(repo_name: str, state) -> List[str]:
+def get_repo_issues(state) -> List[str]:
     """
     Get a list of issues for a specific repository. State can be open or closed.
     """
@@ -75,7 +57,7 @@ def get_repo_issues(repo_name: str, state) -> List[str]:
     return [f"#{i.number} {i.title}  —  by {i.user.login}  [{i.state}]" for i in issues]
 
 @mcp.tool()
-def get_repo_comments(repo_name: str, issue_number: int) -> List[str]:
+def get_repo_comments(issue_number: int) -> List[str]:
     """
     Get comments for a specific issue in a repository.
     """
